a22bb5645233b544013b813c1ad165ecc87790b1
sajeewa garusinghe <sajeeva616@gmail.com>
2023-03-29 12:05:37 +0530

    end a message to all users in the room, apart from the user that just joined, to notify them that a new user has joined

--- server/index.js
+++ server/index.js
@@ -10,24 +10,30 @@ const server = http.createServer(app);
 
 // Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
 const io = new Server(server, {
-    cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST'],
-    },
-  });
-
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+const CHAT_BOT = 'ChatBot';
 // Listen for when the client connects via socket.io-client
 io.on('connection', (socket) => {
-    console.log(`User connected ${socket.id}`);
-  
-    // We can write our socket event listeners in here...
-     // Add a user to a room
+  console.log(`User connected ${socket.id}`);
+
+  // We can write our socket event listeners in here...
+  // Add a user to a room
   socket.on('join_room', (data) => {
     const { username, room } = data; // Data sent from client when join_room event emitted
     socket.join(room); // Join the user to a socket room
-  });
-  });
 
-
+    let __createdtime__ = Date.now(); // Current timestamp
+    // Send message to all users currently in the room, apart from the user that just joined
+    socket.to(room).emit('receive_message', {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+  });
+});
 
 server.listen(4000, () => 'Server is running on port 4000');
