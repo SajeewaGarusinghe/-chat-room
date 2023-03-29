53b638c58cddee60ab3f23b42b53d6f2caaa6664
sajeewa garusinghe <sajeeva616@gmail.com>
2023-03-29 12:01:01 +0530

    join the user to a Socket.io room

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # -chat-room
+
 we will be using Socket.io and HarperDB to build a fullstack, real-time chat application with chat rooms
+
+server side dependencies
+
+npm i axios cors express socket.io dotenv
+npm i   express   dotenv
+npm i -D nodemon
+
+    Axios is a commonly used package to easily make requests to APIs.
+    Cors allows our client to make requests to other origins – necessary for socket.io to work properly.Cross-Origin Resource Sharing
+    Express is a NodeJS framework that allows us to write our backend more easily with less code.
+    Socket.io is a library that allows the client and server to communicate in realtime – which isn't possible with standard HTTP requests.
+    Dotenv is a module that allows us to store private keys and passwords safely, and load them into our code when needed.
+    install nodemon as a dev dependency, so we don't have to restart our server every time we make a change to the code – saving us time and energy:
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors()); // Add cors middleware
+
+const server = http.createServer(app);
+
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+  
+    // We can write our socket event listeners in here...
+     // Add a user to a room
+  socket.on('join_room', (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+  });
+  });
+
+
+
+server.listen(4000, () => 'Server is running on port 4000');
